d01c24a042e104b75d876bc563bc0455f55ad1e7
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-09-27 00:32:55 -0300

    feat: create design token css structure and add to global styles

---
+++ src/styles/designTokens/animations.tsx
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+export const Animations = css`
+  @keyframes smoothFadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
-
 import githubBackground from 'assets/github-background.svg';
+import { Colors, Typografy, Layout, Animations } from './designTokens';
 
 export default createGlobalStyle`
+  ${Colors};
+  ${Typografy};
+  ${Layout};
+  ${Animations};
 
   *{
     margin:0;
@@ -10,45 +14,14 @@ export default createGlobalStyle`
     outline:0;
     box-sizing: border-box;
   }
-  :root{
-    --background:#f0f0f5;
-    --secondary:#3a3a3a;
-    --primary:#04d361;
-    --tertiary:#fff;
-    --font-regular:1.8rem;
-  }
-
-  html{
-    font-size:62.5%;
-  }
 
   body{
     background: url(${githubBackground}) no-repeat 70% top;
-
-      background-color:var(---background);
-      -webkit-font-smoothing: antialiased;
+    background-color:var(---background);
+    -webkit-font-smoothing: antialiased;
   }
 
-  body,input, button{
-      font:1.6rem Roboto, sans-serif;
-  }
-
-  #root{
-      max-width:960px;
-      margin:0 auto;
-      padding: 4rem 2rem;
-  }
   button{
       cursor:pointer;
   }
-
-
-  @keyframes smoothFadeIn{
-      from{
-        opacity:0
-      }
-      to{
-        opacity:1;
-      }
-    }
 `;
